26c7ac1b1bf00f78accf3023894586c986d64046
Benjamin Gale <benjaminiangale@gmail.com>
2026-05-23 17:25:34 +0100

    Tidy up StatusBarView

--- example/src/main/java/mvvm/example/shell/main/statusbar/StatusBarView.java
+++ example/src/main/java/mvvm/example/shell/main/statusbar/StatusBarView.java
@@ -14,36 +14,36 @@ import mvvm.example.core.view.controls.Spacer;
 public class StatusBarView extends BorderPane {
 
     private final ViewLocator<Region> viewLocator;
-    private final HBox content = new HBox();
+    private final HBox content = content();
 
     public StatusBarView(StatusBarViewModel viewModel, ViewLocator<Region> viewLocator) {
         this.viewLocator = viewLocator;
-
-        content.setPadding(new Insets(5, 8, 5, 8));
-        content.setMinHeight(24);
-        content.setSpacing(8);
 
         setTop(new Separator());
         setCenter(content);
 
         setContent(viewModel.statusItems());
 
-        viewModel
-            .statusItems()
+        viewModel.statusItems()
             .addListener((ListChangeListener<StatusItemViewModel>) _ ->
-                setContent(viewModel.statusItems())
-            );
+                setContent(viewModel.statusItems()));
     }
 
     private void setContent(ObservableList<StatusItemViewModel> statusItems) {
         var children = content.getChildren();
-
         children.clear();
         children.add(Spacer.create());
-
         for (int i = 0; i < statusItems.size(); i++) {
             if (i > 0) children.add(new Separator(Orientation.VERTICAL));
             children.add(viewLocator.locate(statusItems.get(i)));
         }
+    }
+
+    private static HBox content() {
+        var content = new HBox();
+        content.setPadding(new Insets(5, 8, 5, 8));
+        content.setMinHeight(24);
+        content.setSpacing(8);
+        return content;
     }
 }
